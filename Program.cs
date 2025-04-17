386010168d37d685203f1b3e63f87bef55667640
Christopher Williams <williamstophef@gmail.com>
2025-04-17 09:26:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Week2_Challenge_Labs4;
+
+bool continueProgram = true;
+char userChar;
+
+while (continueProgram)
+{
+    Student student1 = new Student();
+
+    Console.WriteLine("Enter student information: ");
+    Console.WriteLine("Roll Number: ");
+    student1.RollNumber = Console.ReadLine();
+    Console.WriteLine("Student name: ");
+    student1.StudentName = Console.ReadLine();
+    Console.WriteLine("Physics grade: ");
+    student1.PhysicsGrade = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Chemistry grade: ");
+    student1.ChemistryGrade = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Computer grade: ");
+    student1.ComputerGrade = Convert.ToDouble(Console.ReadLine());
+
+    student1.CalculateTotal();
+    student1.CalculatePercent();
+    student1.CalculateDivision();
+
+    Console.WriteLine($"""
+
+    Roll Number:                   {student1.RollNumber}
+    Name of student:               {student1.StudentName}
+    Marks in Physics:              {student1.PhysicsGrade}
+    Marks in Chemistry:            {student1.ChemistryGrade}
+    Marks in Computer Application: {student1.ComputerGrade}
+    Total marks:                   {student1.Total}
+    Percentage:                    {student1.Percent.ToString("P")}
+    Division:                      {student1.Division}
+
+    """);
+
+    Console.WriteLine("Do you have another student to enter?");
+    Console.WriteLine("y for yes: n for no");
+    userChar = Convert.ToChar(Console.ReadLine());
+
+    if (userChar == 'n')
+    {
+        Console.WriteLine("Good Bye");
+        continueProgram = false;
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿namespace Week2_Challenge_Labs4
+{
+    internal class Student
+    {
+        public string Division { get; set; }
+        public string RollNumber { get; set; }
+        public string StudentName { get; set; }
+        public double ChemistryGrade { get; set; }
+        public double ComputerGrade { get; set; }
+        public double Percent { get; set; }
+        public double PhysicsGrade { get; set; }
+        public double Total { get; set; }
+
+        public double CalculateTotal()
+        {
+            return Total = PhysicsGrade + ChemistryGrade + ComputerGrade;
+        }
+
+        public double CalculatePercent()
+        {
+            return Percent = Total / 300;
+        }
+
+        public void CalculateDivision()
+        {
+            switch (Percent)
+            {
+                case > .6:
+                    Division = "Division 1";
+                    break;
+                case < .6 and > .45:
+                    Division = "Division 2";
+                    break;
+                case < .45 and > .33:
+                    Division = "Division 3";
+                    break;
+            }
+        }
+    }
+}
